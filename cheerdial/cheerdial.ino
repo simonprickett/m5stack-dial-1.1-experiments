9f35fa0bca1fb094f9a470ac82e4dab88253b97d
Simon Prickett <simon.prickett@grafana.com>
2025-07-22 15:18:00 +0100

    Added color names, redid implementation.

--- cheerdial/cheerdial.ino
+++ cheerdial/cheerdial.ino
@@ -2,33 +2,42 @@
 
 short NUM_COLORS = 11;
 long oldPosition = -999;
-// TODO set up the colors...
 
-uint32_t colors[11];
+struct CHEERLIGHT {
+    int r;
+    int g;
+    int b;
+    char *colorName;
+    // TODO add text color stuff.
+};
+
+struct CHEERLIGHT cheerlights[] = {
+    { 255, 0, 0, "red" },       // OK
+    { 0, 255, 0, "green" },     // OK
+    { 0, 0, 255, "blue" },      // OK
+    { 0, 255, 255, "cyan" },    // OK
+    { 255, 255, 255, "white" }, // OK
+    { 253, 245, 240, "oldlace" }, // Too similar to white...
+    { 128, 0, 128, "purple" },    // OK
+    { 255, 0, 255, "magenta" },   // Too similar to purple...
+    { 255, 255, 0, "yellow" },    // OK
+    { 255, 165, 0, "orange" },    // OK
+    { 255, 192, 203, "pink" }     // Too light not pink enough!
+};
+
 short idx = 0;
+
+M5GFX display;
+M5Canvas canvas(&display);
 
 void setup() {
     Serial.begin(9600);
     auto cfg = M5.config();
     // true, false = Enable encoder, disable RFID.
     M5Dial.begin(cfg, true, false);
-    // TODO some other setup stuff.
-    M5Dial.Display.setTextFont(&fonts::Orbitron_Light_32);
-    M5Dial.Display.setTextSize(2);
 
+    display.begin();
     // Some other stuff...
-    // TODO declare literal array more obviously?
-    colors[0] = M5Dial.Display.color888(255, 0, 0); // Red OK
-    colors[1] = M5Dial.Display.color888(0, 255, 0); // Green OK
-    colors[2] = M5Dial.Display.color888(0, 0, 255); // Blue OK
-    colors[3] = M5Dial.Display.color888(0, 255, 255); // Cyan OK
-    colors[4] = M5Dial.Display.color888(255, 255, 255); // White OK
-    colors[5] = M5Dial.Display.color888(253, 245, 240); // Oldlace needs better distinguish from white
-    colors[6] = M5Dial.Display.color888(128, 0, 128); // Purple OK
-    colors[7] = M5Dial.Display.color888(255, 0, 255); // Magenta not very distinguished from purple?
-    colors[8] = M5Dial.Display.color888(255, 255, 0); // Yellow OK
-    colors[9] = M5Dial.Display.color888(255, 165, 0); // Orange OK
-    colors[10] = M5Dial.Display.color888(255, 192, 203); // Pink very light needs more pinkness!
 }
 
 void loop() {
@@ -36,26 +45,27 @@ void loop() {
     
     long newPosition = M5Dial.Encoder.read();
     if (newPosition != oldPosition) {
-        // Position has changed!
+        // Position has changed! 
+        // TODO consider only updating after a couple of position clicks?
         M5Dial.Speaker.tone(8000, 20);
-        M5Dial.Display.clear();
-
-        //Serial.println(newPosition);
+        display.clear();
+        display.startWrite();
+        canvas.createSprite(M5Dial.Display.width(), M5Dial.Display.height());
+        canvas.fillSprite(M5Dial.Display.color888(cheerlights[idx].r, cheerlights[idx].g, cheerlights[idx].b));
+        canvas.setTextColor(M5Dial.Display.color888(0, 0, 0));
+        canvas.setTextDatum(middle_center);
+        canvas.setFont(&fonts::Orbitron_Light_32);
+        canvas.setTextSize(1);
+        canvas.drawString(cheerlights[idx].colorName, M5Dial.Display.width() / 2, M5Dial.Display.height() / 2);
+        canvas.pushSprite(0, 0);
+        display.endWrite();
 
         // Some of the things below are constants.
-        // How do colors work?
         // Should I clear the screen between things.
-        // What are the bouncs of newPosition?
+        // What are the bounds of newPosition?
         // Sort out the save location here... in cheerdial folder.
     
-        int x      = M5Dial.Display.width() / 2;
-        int y      = M5Dial.Display.height() / 2;
-        int r      = x;
-        //uint16_t c = M5Dial.Display.color888(255, 128, 0);
-        M5Dial.Display.clear();
-        //Serial.println(colors[idx]);
         Serial.println(idx);
-        M5Dial.Display.fillCircle(x, y, r, colors[idx]);
 
         // TODO this will need fixing so it starts on idx 0.
         // TODO also needs fixing for both directions.
